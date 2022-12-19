9b3a0fbb5a03b0bafd17c7841f6d7ba62e9d7f45
unknown <christopherbonime@gmail.com>
2022-12-19 15:40:26 -0500

    added questions for user input in index.js

--- index.js
+++ index.js
@@ -3,7 +3,64 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 const generateMarkdown = require('./utils/generateMarkdown.js');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [{
+    type: 'input',
+    name: 'title',
+    message: 'What is the title of your project?'
+},
+{
+    type: 'input',
+    name: 'description',
+    message: 'Write a description of the What, Why, and How your project works along with inspiration on creating the project.'
+},
+{
+    type: 'input',
+    name: 'installation',
+    message: 'Give an explanation (if needed) on how to install your application.'
+},
+{
+    type: 'input',
+    name: 'usage',
+    message: 'Short descripted explanation on how to use your application.'
+},
+{
+    type: 'input',
+    name: 'collaborators',
+    message: 'Add any sources or contributors on your application.'
+},
+{
+    type: 'input',
+    name: 'tests',
+    message: 'Write possible ways someone can test out your application.'
+},
+{
+    type: 'input',
+    name: 'Description',
+    message: 'Write a description of the What, Why, and How your project works along with inspiration on creating the project.'
+},
+{
+    type: 'list',
+    name: 'license',
+    message: 'Select a license you wish to use for your application.',
+    choices: ['GNU GPLv3', 'GNU AGPLv3', 'GNU LGPLv3', 'MIT License', 'Apache License 2.0', 'The Unlicense'],
+    default: 'MIT License'
+},
+{
+    type: 'input',
+    name: 'github',
+    message: 'Enter your Github username.'
+},
+{
+    type: 'input',
+    name: 'email',
+    message: 'Write is your Email?'
+},
+{
+    type: 'input',
+    name: 'Description',
+    message: 'Write a description of the What, Why, and How your project works along with inspiration on creating the project.'
+}
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}

b24f417738baf92be5e35a5f717a3d0dbaaf0efc
unknown <christopherbonime@gmail.com>
2022-12-19 16:51:51 -0500

    created switch case function for render license link depending on the license the user selects to display corresponding badge icon along with case for an empty string is no license is selected

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -10,13 +10,26 @@ function renderLicenseBadge(license) {
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  switch (license) {
+    case 'GNU GPLv3': return 'https://img.shields.io/badge/License-GPLv3-blue.svg';
+    case 'MIT License': return 'https://img.shields.io/badge/License-MIT-yellow.svg';
+    case 'Apache License 2.0': return 'https://img.shields.io/badge/License-Apache_2.0-blue.svg';
+    case 'The Unlicense': return 'https://img.shields.io/badge/license-Unlicense-blue.svg';
+    case '': return ''
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+}
 
 // TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {}
+function generateMarkdown(data) {
+  return `# ${data.title}
+
+`;
+}
 
 module.exports = generateMarkdown;
